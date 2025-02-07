9c942834d1120d7c8605a78eac3e91b25e087b43
MateusLecchi20 <mateusrlecchi@gmail.com>
2025-02-07 10:13:20 -0300

    Secao 4: Angular

---
+++ Front/ProEventos-App/.angular/cache/19.1.5/ProEventos-App/vite/deps_ssr/@angular_platform-browser_animations.js
@@ -0,0 +1,23 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  BrowserAnimationsModule,
+  InjectableAnimationEngine,
+  NoopAnimationsModule,
+  provideAnimations,
+  provideNoopAnimations
+} from "./chunk-MYLEEFTG.js";
+import "./chunk-FZHFUH5J.js";
+import "./chunk-RSDZCMNU.js";
+import "./chunk-YAGPW5AS.js";
+import {
+  ANIMATION_MODULE_TYPE
+} from "./chunk-53Z7NWHP.js";
+import "./chunk-YHCV7DAQ.js";
+export {
+  ANIMATION_MODULE_TYPE,
+  BrowserAnimationsModule,
+  NoopAnimationsModule,
+  provideAnimations,
+  provideNoopAnimations,
+  InjectableAnimationEngine as ɵInjectableAnimationEngine
+};
